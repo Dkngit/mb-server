989b0300af54abb49756729227d4b90dec96b9e5
Dkngit <lhc2014v@outlook.com>
2019-12-04 23:37:08 +0800

    登录接口返回正确用户信息

--- src/main/java/com/example/mbserver/web/UsersController.java
+++ src/main/java/com/example/mbserver/web/UsersController.java
@@ -2,13 +2,14 @@ package com.example.mbserver.web;
 
 import com.example.mbserver.dao.UserPageRepository;
 import com.example.mbserver.domain.User;
+import com.example.mbserver.security.MBUserDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.security.Principal;
 
 @RestController
 @RequestMapping("api/user")
@@ -17,10 +18,15 @@ public class UsersController {
     @Resource
     private UserPageRepository userPageRepository;
 
-    @RequestMapping("own")
-    public char own(Principal principal) {
-//        principal.
-        return 's';
+    @RequestMapping("login")
+    public User login(@AuthenticationPrincipal MBUserDetails userDetails) {
+//        Authentication authentication = (Authentication) principal;
+//        authentication.getPrincipal();
+
+        User user = userDetails.getUser();
+        user.setPassword(null);
+
+        return user;
     }
 
     @RequestMapping("list")
