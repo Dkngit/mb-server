e97ea6c6248e0fd29dbe22dada001b0e9f0c4ace
Dkngit <lhc2014v@outlook.com>
2020-01-07 23:36:21 +0800

    用户列表(排除ADMIN)接口，保存接口

--- src/main/java/com/example/mbserver/dao/UserPageRepository.java
+++ src/main/java/com/example/mbserver/dao/UserPageRepository.java
@@ -1,9 +1,13 @@
 package com.example.mbserver.dao;
 
 import com.example.mbserver.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserPageRepository extends PagingAndSortingRepository<User, Long> {
 
     User findUserByUsername(String username);
+
+    Page<User> findAllByRolesContains(String role, Pageable pageable);
 }
--- src/main/java/com/example/mbserver/web/UsersController.java
+++ src/main/java/com/example/mbserver/web/UsersController.java
@@ -2,9 +2,9 @@ package com.example.mbserver.web;
 
 import com.example.mbserver.dao.UserPageRepository;
 import com.example.mbserver.entity.User;
+import com.example.mbserver.pojo.MBPageRequest;
 import com.example.mbserver.security.MBUserDetails;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.security.Principal;
+import java.util.Date;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/user")
@@ -42,10 +45,24 @@ public class UsersController {
     }
 
     @RequestMapping("list")
-    public Page<User> list(int pageIndex, int pageSize) {
-        System.out.println(pageIndex);
-        System.out.println(pageSize);
-        return userPageRepository.findAll(PageRequest.of(pageIndex, pageSize));
+    public Page<User> list(@RequestBody MBPageRequest pageR) {
+        return userPageRepository.findAllByRolesContains("ROLE_USER", pageR.pageable());
+    }
+
+    @RequestMapping("save")
+    public void save(@RequestBody User user, Principal principal) {
+        Date now = new Date();
+        String name = principal.getName();
+        if (user.getId() == 0) {
+            user.setCreateOn(now);
+            user.setCreateBy(name);
+            Set<String> roles = user.getRoles();
+            roles.add("ROLE_USER");
+        } else {
+            user.setModifyOn(now);
+            user.setModifyBy(name);
+        }
+        userPageRepository.save(user);
     }
 
 
